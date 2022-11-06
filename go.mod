bd280da609e399f2379bba590242c9226395d878
bachtiar firdaus <bachtiar.firdaus@dansmultipro.com>
2022-11-06 13:12:52 +0700

    Belajar Golang Membuat module say-hello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Bachtiar-Firdaus/belajar-golang-modules
+
+go 1.19
---
+++ say_hello.go
@@ -0,0 +1,6 @@
+package go_say_hello
+
+// huruf besar pertama capital menandakan ini function public
+func SayHello() string {
+	return "hello Word !!"
+}
